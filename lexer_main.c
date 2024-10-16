936918f92e7619b314a23713ffa827fdd09af693
MarkMango21 <mark2asf@gmail.com>
2024-10-16 16:25:14 -0700

    one little update to the lexer_main.c file

--- lexer_main.c
+++ lexer_main.c
@@ -1,14 +1,20 @@
 #include <lexer.h>
-
+#include <utilities.h>
 /**
  * A driver program that simply calls provided functions
  * to start the process of lexing a file
  */
 
-int main(char *argv[])
+int main(int argc, char *argv[])
 {
     // A dummy version of the code
-    
+    // A dummy version of the code
+
+    if (argc < 2)
+    {
+        bail_with_error("Usage: %s [-p] <bof_file>", argv[0]);
+    }
+
     // pass the file name to the lexer
     // from the command line
     lexer_init(argv[1]);
